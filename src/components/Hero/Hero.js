1db18efe6e59cb1b5b59961ac5e28e289638edd1
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2023-04-22 04:41:07 -0700

    create search reducer

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { setSearchTerm } from '../../actions';
 import './Hero.css';
 
 const Hero = () => {
+  const [inputValue, setInputValue] = useState('');
+  const dispatch = useDispatch();
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    dispatch(setSearchTerm(inputValue));
+  };
+
   return (
     <section className="hero">
       <div className="hero-text-container">
         <div className="search-container">
-          <form className="myform">
-            <input type="text" placeholder="Search for articles..." />
+          <form className="myform" onSubmit={handleSearch}>
+            <input
+              type="text"
+              placeholder="Search for articles..."
+              value={inputValue}
+              onChange={(e) => setInputValue(e.target.value)}
+            />
             <button type="submit">Search</button>
           </form>
         </div>
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,14 @@
+import { combineReducers } from '@reduxjs/toolkit';
 import promptReducer from './promptReducer';
 import temperatureReducer from './temperatureReducer';
 import lengthReducer from './lengthReducer';
+import searchReducer from './searchReducer';
 
-export {
-  promptReducer,
-  temperatureReducer,
-  lengthReducer,
-};
+const rootReducer = combineReducers({
+  prompt: promptReducer,
+  temperature: temperatureReducer,
+  length: lengthReducer,
+  search: searchReducer,
+});
+
+export default rootReducer;
