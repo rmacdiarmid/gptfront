2f24bee9e7bb455fc9720a5dc5796f417cdcb8a8
Ryan MacDiarmid <ryanmacdiarmid@gmail.com>
2023-04-22 17:30:04 -0700

    Hero image works

--- src/index.js
+++ src/index.js
@@ -5,7 +5,8 @@ import { ApolloProvider } from '@apollo/client';
 import client from './apolloClient'; // Import the client
 import { Provider } from 'react-redux';
 import store from './store';
-
+import reportWebVitals from './reportWebVitals';
+import './index.css'; // Import the index.css file
 
 ReactDOM.render(
   <ApolloProvider client={client}> {/* Pass the client to ApolloProvider */}
@@ -17,3 +18,8 @@ ReactDOM.render(
   </ApolloProvider>,
   document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
